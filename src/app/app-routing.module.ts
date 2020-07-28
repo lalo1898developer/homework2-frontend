07e8d35a7b99c47606aad5f6c37f4935ca08426c
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-28 13:14:21 -0500

    Crea las rutas de la aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TableComponent } from './table/table.component';
+import { GraphicComponent } from './graphic/graphic.component';
+
+
+const routes: Routes = [
+  { path: '', component:  TableComponent, pathMatch: 'full' },
+  { path: 'graphic', component:  GraphicComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
